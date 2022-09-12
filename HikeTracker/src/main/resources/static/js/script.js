71e6f22328cb6d48ea947a7248d7e1f6354eeea9
diegoescutia <escutia.diego@gmail.com>
2022-09-11 21:46:47 -0400

    update

--- HikeTracker/src/main/resources/static/js/script.js
+++ HikeTracker/src/main/resources/static/js/script.js
@@ -1,15 +1,15 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', function() {
 	console.log('script.js loaded')
 	init();
 });
 
-function init(){
+function init() {
 	loadAllRoutes();
-	
-	document.newRouteForm.addRoute.addEventListener('click', function(e){
-		e.preventDefault();
+	addRoute();
+	document.newRouteForm.addRoute.addEventListener('click', function(evt) {
+		evt.preventDefault();
 		console.log('Adding new film');
-		
+
 		let route = {};
 		route.date = newRouteForm.date.value;
 		route.location = newRouteForm.location.value;
@@ -17,107 +17,135 @@ function init(){
 		route.time = newRouteForm.time.value;
 		route.pace = newRouteForm.pace.value;
 		route.description = newRouteForm.description.value;
-		
+
 		addRoute(route);
+
+
 	});
-	
-	
-	
-	
+
+
+
+
 }
 
-function loadAllRoutes(){
+function loadAllRoutes() {
 	let xhr = new XMLHttpRequest();
 	xhr.open("GET", "api/routes");
-	xhr.onreadystatechange = function(){
-		if(xhr.readyState === 4){
-			if(xhr.status === 200){
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 200) {
 				displayAllRoutes(JSON.parse(xhr.responseText));
 			}
-			else{
+			else {
 				console.log("Error loading events: " + xhr.status);
 			}
 		}
 	};
-	
+
 	xhr.send();
 }
 
 
 
-function displayAllRoutes(eventList){
+function displayAllRoutes(eventList) {
+
+
 	let dataDiv = document.getElementById("eventList")
 	dataDiv.textContent = '';
-	
+
 	let ol = document.createElement('ol');
 	dataDiv.appendChild(ol);
-	
-	
-	
-	for (let route of eventList){
+
+
+
+	for (let route of eventList) {
 		let li = document.createElement('li');
 		li.textContent = route.date;
 		ol.appendChild(li);
-		
-		let ul = document.createElement('ul');
-		li.appendChild(ul);
-		
-		li = document.createElement('li');
-		li.textContent = "Location: " + route.location;
-		ul.appendChild(li)
-		
-		li = document.createElement('li');
-		li.textContent ="Distance: " + route.distance+" miles";
-		ul.appendChild(li)
-		
-		li = document.createElement('li');
-		li.textContent = "Time: " + route.time;
-		ul.appendChild(li)
-		
-		li = document.createElement('li');
-		li.textContent = "Pace: " + route.pace + " minutes/mile";
-		ul.appendChild(li)
-		
-		li = document.createElement('li');
-		li.textContent = "Description: " + route.description;
-		ul.appendChild(li)
-		
+
+		li.addEventListener('click', function(evt) {
+			evt.preventDefault();
+
+			let dataDiv = document.getElementById('detail');
+			dataDiv.textContent = '';
+
+			let ul = document.createElement('ul');
+			dataDiv.appendChild(ul)
+
+			let li = document.createElement('li');
+			li.textContent = 'Date: ' + route.date;
+			ul.appendChild(li);
+
+			li = document.createElement('li');
+			li.textContent = 'Location: ' + route.location;
+			ul.appendChild(li);
+
+			li = document.createElement('li');
+			li.textContent = 'Distance ' + route.distance + ' miles';
+			ul.appendChild(li);
+
+			li = document.createElement('li');
+			li.textContent = "Time: " + route.time;
+			ul.appendChild(li)
+
+			li = document.createElement('li');
+			li.textContent = "Pace: " + route.pace + " minutes/mile";
+			ul.appendChild(li)
+
+			li = document.createElement('li');
+			li.textContent = "Description: " + route.description;
+			ul.appendChild(li)
+			
+			updateRoute(route);
+
+		});
+
+
+
+
+
 	}
+
+
+
+
+
 }
+
 
 
 
 function displayRoute(route) {
 	let dataDiv = document.getElementById('routeData');
 	dataDiv.textContent = '';
-	
+
 	let date = document.createElement('h3');
 	date.textContent = "Hike was Succesfully Logged";
 	dataDiv.appendChild(date);
-	
+
 	let ul = document.createElement('ul');
 	dataDiv.appendChild(ul)
-	
+
 	let li = document.createElement('li');
 	li.textContent = 'Date: ' + route.date;
 	ul.appendChild(li);
-	
+
 	li = document.createElement('li');
 	li.textContent = 'Location: ' + route.location;
 	ul.appendChild(li);
-	
+
 	li = document.createElement('li');
 	li.textContent = 'Distance ' + route.distance;
 	ul.appendChild(li);
-	
+
 	li = document.createElement('li');
 	li.textContent = "Time: " + route.time;
 	ul.appendChild(li);
-	
+
 	li = document.createElement('li');
 	li.textContent = "Pace: " + route.pace + " minutes/mile";
 	ul.appendChild(li)
-	
+
 	let desc = document.createElement('blockquote');
 	desc.textContent = "Description: " + route.description;
 	dataDiv.appendChild(desc);
@@ -146,11 +174,15 @@ function addRoute(route) {
 	xhr.send(routeJson);
 }
 
-function updateRoute(route){
-	
+
+
+
+
+function updateRoute(route) {
+
 	let tbody = document.getElementById('routeTable')
 	tbody.textContent = '';
-	
+
 	let updateForm = document.createElement('form');
 	updateForm.setAttribute('name', 'updateForm');
 	let div = document.getElementById('routeTableDiv');
@@ -164,7 +196,7 @@ function updateRoute(route){
 	updateForm.appendChild(routeDate);
 	updateForm.appendChild(dateInput);
 	updateForm.appendChild(document.createElement('br'));
-	
+
 	let routeLocation = document.createElement('label');
 	routeLocation.textContent = "Location: "
 	let locationInput = document.createElement('input');
@@ -173,7 +205,7 @@ function updateRoute(route){
 	updateForm.appendChild(routeLocation);
 	updateForm.appendChild(locationInput);
 	updateForm.appendChild(document.createElement('br'));
-	
+
 	let routeDistance = document.createElement('label');
 	routeDistance.textContent = "Distance: "
 	let distanceInput = document.createElement('input');
@@ -183,7 +215,7 @@ function updateRoute(route){
 	updateForm.appendChild(routeDistance);
 	updateForm.appendChild(distanceInput);
 	updateForm.appendChild(document.createElement('br'));
-	
+
 	let routeTime = document.createElement('label');
 	routeTime.textContent = "Time: "
 	let timeInput = document.createElement('input');
@@ -193,7 +225,7 @@ function updateRoute(route){
 	updateForm.appendChild(routeTime);
 	updateForm.appendChild(timeInput);
 	updateForm.appendChild(document.createElement('br'));
-	
+
 	let routePace = document.createElement('label');
 	routePace.textContent = "Pace: "
 	let paceInput = document.createElement('input');
@@ -203,32 +235,33 @@ function updateRoute(route){
 	updateForm.appendChild(routePace);
 	updateForm.appendChild(paceInput);
 	updateForm.appendChild(document.createElement('br'));
-	
+
 	let routeDescription = document.createElement('label');
 	routeDescription.textContent = "Description: "
-	let routeInput = document.createElement('input');
-	routeInput.setAttribute('name', 'route');
-	routeInput.setAttribute('value', route.description);
-	routeInput.setAttribute('type', 'textarea');
+	let descriptionInput = document.createElement('input');
+	descriptionInput.setAttribute('name', 'route');
+	descriptionInput.setAttribute('value', route.description);
+	descriptionInput.setAttribute('type', 'textarea');
 	updateForm.appendChild(routeDescription);
 	updateForm.appendChild(descriptionInput);
 	updateForm.appendChild(document.createElement('br'));
-	
+
 	let button = document.createElement('button');
 	button.textContent = "Update Hike";
 	button.setAttribute('id', 'updateRoute');
 	button.addEventListener('click', function(e) {
-		e.preventDefault();
+		//e.preventDefault();
 
-	
-		let updateRoute = {};
-		updateRoute.date = updateForm.date.value;
-		updateRoute.location = updateForm.location.value;
-		updateRoute.distance = updateForm.distance.value;
-		updateRoute.time = updateForm.time.value;
-		updateRoute.pace = updateForm.pace.value;
-		updateRoute.description = updateForm.description.value;
+
+		let updateRoute = {
 		
+		date: updateForm.date,
+		location: updateForm.location,
+		distance: updateForm.distance,
+		time: updateForm.time,
+		pace: updateForm.pace, 
+		description: updateForm.description
+};
 		sendUpdate(updateRoute);
 		updateForm.textContent = '';
 	});
@@ -238,20 +271,19 @@ function updateRoute(route){
 
 function sendUpdate(route) {
 	let xhr = new XMLHttpRequest();
-	//xhr.open('PUT', 'api/food');
-	xhr.open('PUT', `api/food/${food.id}`);
+	xhr.open('PATCH', `api/routes/${route.id}`);
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState === 4) {
 			if (xhr.status < 200) {
-				let food = JSON.parse(xhr.responseText);
-				console.log('Updated food');
+				let route = JSON.parse(xhr.responseText);
+				console.log('Updated Hike');
 			} else {
 				console.log('Error updating food: ' + xhr.status);
 			}
 		}
 	};
 	xhr.setRequestHeader("Content-type", "application/json");
-	xhr.send(JSON.stringify(food));
+	xhr.send(JSON.stringify(route));
 }
 
 
